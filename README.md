ab3d12ec96d2dcab7b7c9c71952130002d40407f
bwaklog <aditya.mh@outlook.com>
2023-11-16 03:04:54 +0530

    Remaking Full App 1. Deleted all previous files 2. Made all basic routes for the following - registering/signin - home page - logout - error pages

--- README.md
+++
@@ -1,7 +0,0 @@
-# pleasebekind 🐈
-mini project for end semester python project
-
-contributors :
-- [Aditya Rao](https://github.com/AdiXgit) 👋
-- [Aditya Hegde](https://bwaklog.github.io) 👋
-- [Lalith Reddy](https://github.com/Lalith2907) 👋
--- app.py
+++ app.py
@@ -1,62 +1,68 @@
-from flask import Flask, render_template, url_for, request, redirect, flash
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
-import time
+from flask import Flask, request, render_template, redirect, session
+from flask_session import Session
+import regex as re
 
 app = Flask(__name__)
-app.secret_key = 'pleasebekind'.encode('utf-16')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///post.db'
-db = SQLAlchemy(app)
 
-class Post(db.Model):
-    post_id = db.Column(db.Integer, primary_key=True, nullable=False)
-    post_user_handle = db.Column(db.Integer, nullable=False)
-    post_content = db.Column(db.String(200), nullable=False)
-    post_timestamp = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+# if flase, if we quit browser the session is deleted
+app.config["SESSION_PERMANENT"] = False
+# stored in servers files and not the cookies
+app.config["SESSION_TYPE"] = "filesystem"
+# Activate session
+Session(app)
 
-uid = ""
-# Authentication Page
-@app.route('/', methods=['POST', 'GET'])
-def auth_signin():
-    if request.method != "POST":
-        return render_template('auth-signin.html')
-    if len(request.form['user_handle']) > 1:
-        uid = request.form['user_handle']
-    else:
-        flash("Username Missing 😔", category='message')
-        return redirect('/')
-    return redirect(f'home/{uid}')
+# Implement a database here and define the schema
 
-@app.route('/signup/', methods=["POST", "GET"])
-def auth_signup():
+
+@app.route("/")
+def index():
+    # default method is GET
+    # need to check if we are logged into the session
+    username = session.get("username")
+    return render_template("index.html", username=username)
+
+
+@app.route("/signin", methods=["GET", "POST"])
+def signin():
+    # validates inputs and checks if login in is possible
     if request.method == "POST":
-        return redirect('/')
-    else:
-        return render_template('auth_signup.html')
 
-# Home page
-# This is where the content is going to be visiable
-@app.route('/home/<uid>', methods=['POST', 'GET'])
-def home(uid):
-    if request.method == 'POST':
-        if len(request.form['content']) > 1:
-            post_content = request.form['content']
-        else:
-            flash("You can't have an empty post 😔", category='message')
-            return redirect(f'/home/{uid}')
-        new_post = Post(post_content=post_content, post_user_handle=uid)
-        try:
-            db.session.add(new_post)
-            db.session.commit()
-            return redirect(f'/home/{uid}')
-        except Exception:
-            return 'There was an issue sending the post'
-    else:
-        print("Rendering Home Page")
-        posts = Post.query.order_by(Post.post_timestamp.desc()).all()
-        return render_template('home.html', posts=posts, uid=uid)
+        # these checks below will not be necessary at all times
+        # if an intruder tries editing the source file by removing the required filed, 
+        # this code will make sure that there isnt a username or password with null value
+
+        # check for username validity
+        username = request.form.get("username")
+        if not request.form.get("username"):
+            return render_template("error.html", message="Invalid Username")
+        password = request.form.get("password")
+        if not request.form.get("password"):
+            return render_template("error.html", message="Invalid Password")
+        # check for password validity
+
+        # here we add the homepage route, and before which add the username to session
+        session["username"] = username
+        session["password"] = password
+
+        # all credentials satisfied so redirecting to /home
+        return redirect("/home")
+    # throw you back to / route if you arent signed in
+    return "Signin Route"
+
+@app.route("/logout")
+def logout():
+    # default method is get
+    session.clear()
+    # redirects back to home page once the 
+    # session has been cleared
+    return redirect('/')
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# route to home page which has a view of all the posts
+@app.route("/home")
+def home():
+    # throw you back to / route if you aren't signed in
+    username = session.get("username")
+    if username:
+        return render_template("home.html", username=username)
